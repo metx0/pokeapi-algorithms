104b98db8b36ab4c51188d7ec8f1219723e02e47
CosmicFabri <fabrizzio86@outlook.es>
2023-11-04 12:44:39 -0600

    Archivo de Python 'sort_search_pokemons.py' creado
    Este codigo es la interfaz para el usuario y se va a encargar de llamar a las funciones para ordenar y buscar los elementos

---
+++ sort_search_pokemons.py
@@ -0,0 +1,14 @@
+print("¡Ordenamiento y búsqueda de Pokemons!")
+print()
+print("Opción 1 - Ordenar Pokemons en base a una estadística")
+print("Opción 2 - Buscar un Pokemon para ver sus estadísticas")
+print()
+opcion = int(input("Digita tu elección: "))
+print()
+
+if opcion == 1:
+    print("Estás ordenando Pokemones test :)")
+elif opcion == 2:
+    print("Estás buscando un Pokemon")
+else:
+    print("Opción inválida")
